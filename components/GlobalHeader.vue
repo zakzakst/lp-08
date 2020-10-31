bdb1dfdf45d23edbb397040851c6fb413fa39516
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-11-01 03:30:06 +0900

    HOMEページの作成

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-v-app-bar(dark fixed)
-  v-tabs(centered center-active show-arrows)
+v-app-bar(dark,fixed)
+  v-tabs(centered,center-active,show-arrows)
     v-tab(
-      v-for="(item, index) in tabItems"
+      v-for="(item, index) in tabItems",
       :key="index"
     ) {{ item.label }}
 </template>
@@ -11,12 +11,10 @@ v-app-bar(dark fixed)
 export default {
   data: () => ({
     tabItems: [
-      { label: 'Tab 1' },
-      { label: 'Tab 2' },
-      { label: 'Tab 3' },
-      { label: 'Tab 4' },
-      { label: 'Tab 5' },
-      { label: 'Tab 6' },
+      { label: 'HOME' },
+      { label: '講演一覧' },
+      { label: 'よくある質問' },
+      { label: 'お問い合わせ' },
     ],
   })
 }
